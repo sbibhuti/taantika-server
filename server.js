a6dbad75b62f5a2cd5161ae15a5277143cca02f4
sbibhuti <bibhutisunari@gmail.com>
2026-07-30 05:31:30 +0530

    update cors origin

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://sbibhuti.github.io"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
