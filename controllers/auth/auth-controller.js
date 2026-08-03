5013fe61f360fafc5fbe889f17fd980c94573b75
sbibhuti <bibhutisunari@gmail.com>
2026-08-03 20:57:13 +0530

    cookie content fix 2

--- controllers/auth/auth-controller.js
+++ controllers/auth/auth-controller.js
@@ -6,8 +6,8 @@ const isProd = process.env.NODE_ENV === "production";
 
 const cookieContent = {
   httpOnly: true,
-  secure: false,
-  sameSite: "lax",
+  secure: isProd,
+  sameSite: isProd ? "none" : "lax",
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
 
